fc10264f6b94b54e7112cfe2e426fd3480c86dfe
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2023-04-13 01:01:36 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,3 +16,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
+
+var acc = document.getElementsByClassName("accord-img");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("scale");
+
+    /* Toggle between hiding and showing the active panel */
+   
+  });
+}
